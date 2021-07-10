84e18636d12e178b332dd4c0f85e9f953c9418ff
Priya Mora <61647782+Priya-088@users.noreply.github.com>
2021-07-10 15:24:58 +0530

    change1

--- app.py
+++ app.py
@@ -24,36 +24,3 @@ def upload():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
- #Fetching the data from database   
-
-target =  sql.connect(host = 'sql6.freemysqlhosting.net' ,user='sql6423797',password = 'uunBv5sHVq', database = 'sql6423797')
-
-mycursor = target.cursor()
-mycursor.execute('Select * from `Alphaa Superstore Nov 2020.xlsx`')
-records = mycursor.fetchall()
-type(records)
-df = pd.DataFrame(data = records, columns=['index','DaystoShipActual','ShipStatus','DaystoShipScheduled','Category','City','Country','CustomerName','DiscountPercent','OrderDate', 'OrderID','Sales Person','PostalCode',	'ProductId','ProductName','Profit','Quantity','Region','Returned','ProfitperOrder','Sales','Segment','ShipDate','ShipMode','State','SubCategory'])
-df.head()
-del df['index']
-df.head()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
